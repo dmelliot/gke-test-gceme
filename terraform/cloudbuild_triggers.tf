7152aedc4848795057617df71dc13b3aee02b571
Duncan Elliot <42836473+dmelliot@users.noreply.github.com>
2021-03-31 10:08:04 +1100

    Add missing initial Terraform files

---
+++ terraform/cloudbuild_triggers.tf
@@ -0,0 +1,56 @@
+resource "google_cloudbuild_trigger" "trigger-dev" {
+
+  name = "Trigger-DEV"
+
+  trigger_template {
+    project_id  = "duncan-elliot-sandbox"
+    branch_name = "[^(?!.*main)].*"
+    repo_name   = "default"
+    dir         = "app"
+  }
+
+  substitutions = {
+    _CLOUDSDK_COMPUTE_ZONE      = var.region
+    _CLOUDSDK_CONTAINER_CLUSTER = google_container_cluster.primary.name
+  }
+
+  filename = "app/builder/cloudbuild-dev.yaml"
+}
+
+resource "google_cloudbuild_trigger" "trigger-canary" {
+
+  name = "Trigger-CANARY"
+
+  trigger_template {
+    project_id  = "duncan-elliot-sandbox"
+    branch_name = "main"
+    repo_name   = "default"
+    dir         = "app"
+  }
+
+  substitutions = {
+    _CLOUDSDK_COMPUTE_ZONE      = var.region
+    _CLOUDSDK_CONTAINER_CLUSTER = google_container_cluster.primary.name
+  }
+
+  filename = "app/builder/cloudbuild-canary.yaml"
+}
+
+resource "google_cloudbuild_trigger" "trigger-prod" {
+
+  name = "Trigger-PROD"
+
+  trigger_template {
+    project_id = "duncan-elliot-sandbox"
+    tag_name   = ".*"
+    repo_name  = "default"
+    dir        = "app"
+  }
+
+  substitutions = {
+    _CLOUDSDK_COMPUTE_ZONE      = var.region
+    _CLOUDSDK_CONTAINER_CLUSTER = google_container_cluster.primary.name
+  }
+
+  filename = "app/builder/cloudbuild-prod.yaml"
+}
