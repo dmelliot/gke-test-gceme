84d3aa029cff5b86dc01498b71061753e30fc17a
Duncan Elliot <42836473+dmelliot@users.noreply.github.com>
2021-03-30 13:37:52 +1100

    Remove instance, update locks

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,30 @@
 
 // Configure the Google Cloud provider
 provider "google" {
- project     = "duncan-elliot-sandbox"
- region      = "australia-southeast1"
+  project = var.project_id
+  region  = var.region
 }
+
+# resource "random_id" "instance_id" {
+#   byte_length = 8
+# }
+
+# resource "google_compute_instance" "default" {
+#   name         = "flask-vm-${random_id.instance_id.hex}"
+#   machine_type = "f1-micro"
+#   zone         = "us-west1-a"
+
+#   boot_disk {
+#     initialize_params {
+#       image = "debian-cloud/debian-9"
+#     }
+#   }
+
+#   network_interface {
+#     network = "default"
+
+#     access_config {
+#       // Ephemeral IP
+#     }
+#   }
+# }
